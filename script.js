bcdc1723420903e4744992a4777d2658ed7f7482
Tadi Mhizha <151214925+Tadi263@users.noreply.github.com>
2025-02-15 14:28:50 +0000

    updated score system winners annoucement

--- script.js
+++ script.js
@@ -1,45 +1,39 @@
-let computerchoice = ' ';
+let computerchoice = ' '; //declaring computer choice
 function getComputerChoice() {
 	if(Math.random() < 0.3) //the value will have to be smaller than 0.3 for rock to picked
 		{ 
             computerchoice = 'Rock'
-            return computerchoice;
 
         }
 	else if (Math.random() > 0.6) //the value will have to be bigger than 0.6 for paper to picked
 		{ 
             computerchoice = 'Paper'
-            return computerchoice;
-
         }
 	else 				//the value will have to be between 0.3 and 0.6 for scissors to picked
 		{
             computerchoice = 'Scissors'
-            return computerchoice;
         }
+        console.log('Computer chose: ', computerchoice);
 				} 
 
-                getComputerChoice();
-
-
-let humanchoice = '';
+let humanchoice = ''; //declaring human choice
 function getHumanChoice() {
     humanchoice = prompt('Please make a choice between Rock, Paper or Scissors', ); //Prompt user for choice
     humanchoice = humanchoice.trim().toUpperCase();  //Ensure choice is formatted to prevent discrepancies
         if(humanchoice == 'ROCK')
         {
             humanchoice = 'Rock'
-            return humanchoice;
+            console.log('Human chose: ', humanchoice);
         }
         else if (humanchoice == 'PAPER')
         {
             humanchoice = 'Paper'
-            return humanchoice;
+            console.log('Human chose: ', humanchoice);
         }
         else if (humanchoice == 'SCISSORS')
         {
             humanchoice = 'Scissors'
-            return humanchoice;
+            console.log('Human chose: ', humanchoice);
         }
         else{
             console.log('Please enter Rock, Paper or Scissors'); 
@@ -47,45 +41,47 @@ function getHumanChoice() {
         }
                             }
 
-                getHumanChoice();
-                console.log('Human chose: ', humanchoice);
-                console.log('Computer chose: ', computerchoice);
+function playRound(){
+    getComputerChoice();
+    getHumanChoice();
 
-
-
-let humanScore = 0
-let computerScore = 0
-
-if (humanchoice == computerchoice)
+let humanScore = 0 //declaring human score
+let computerScore = 0 //declaring computer score
+//condidtions for scores
+if (humanchoice == computerchoice) 
     {
-        console.log('Draw');
+        console.log('This round is a draw');
         console.log(humanScore);
         console.log(computerScore);
     }
 else if(humanchoice === 'Rock' && computerchoice === 'Scissors')
     {
-        console.log('Human wins');
+        console.log('You win! '+ humanchoice + ' beats ' + computerchoice);
         humanScore ++;
         console.log('Human score: ', humanScore);
         console.log('Computer score: ', computerScore);
     }
 else if(humanchoice === 'Paper' && computerchoice === 'Rock')
     {
-        console.log('Human wins');
+        console.log('You win! '+ humanchoice + ' beats ' + computerchoice);
         humanScore ++;
         console.log('Human score: ', humanScore);
         console.log('Computer score: ', computerScore);
     }
 else if(humanchoice === 'Scissors' && computerchoice === 'Paper')
     {
-        console.log('Human wins');
+        console.log('You win! '+ humanchoice + ' beats ' + computerchoice);
         humanScore ++;
         console.log('Human score: ', humanScore);
         console.log('Computer score: ', computerScore);
     }
 else
     {
+        console.log('You lose! '+ computerchoice + ' beats ' + humanchoice);
         computerScore ++;
         console.log('Human score: ', humanScore);
         console.log('Computer score: ', computerScore);;
     }
+}
+
+playRound();
