20e4b54764b0787f3c8d2296115cccf3f88ae6ce
Tadi Mhizha <151214925+Tadi263@users.noreply.github.com>
2025-02-15 14:02:14 +0000

    fixed getHumanchoice and added score system

--- script.js
+++ script.js
@@ -1,47 +1,91 @@
+let computerchoice = ' ';
 function getComputerChoice() {
 	if(Math.random() < 0.3) //the value will have to be smaller than 0.3 for rock to picked
 		{ 
-            console.log('Rock');
-            return 'Rock';
+            computerchoice = 'Rock'
+            return computerchoice;
 
         }
 	else if (Math.random() > 0.6) //the value will have to be bigger than 0.6 for paper to picked
 		{ 
-            console.log('Paper');
-            return 'Paper';
+            computerchoice = 'Paper'
+            return computerchoice;
 
         }
 	else 				//the value will have to be between 0.3 and 0.6 for scissors to picked
 		{
-            console.log('Scissors');
-            return 'Scissors';
+            computerchoice = 'Scissors'
+            return computerchoice;
         }
 				} 
 
-                getComputerChoice()
+                getComputerChoice();
 
 
+let humanchoice = '';
 function getHumanChoice() {
-    let choice = prompt('Please make a choice between Rock, Paper or Scissors', ); //Prompt user for choice
-    choice = choice.trim().toUpperCase();  //Ensure choice is formatted to prevent discrepancies
-        if(choice == 'ROCK')
+    humanchoice = prompt('Please make a choice between Rock, Paper or Scissors', ); //Prompt user for choice
+    humanchoice = humanchoice.trim().toUpperCase();  //Ensure choice is formatted to prevent discrepancies
+        if(humanchoice == 'ROCK')
         {
-            console.log('Rock');
-            return 'Rock';
+            humanchoice = 'Rock'
+            return humanchoice;
         }
-        else if (choice == 'PAPER')
+        else if (humanchoice == 'PAPER')
         {
-            console.log('Paper');
-            return 'Paper';
+            humanchoice = 'Paper'
+            return humanchoice;
         }
-        else if (choice == 'SCISSORS')
+        else if (humanchoice == 'SCISSORS')
         {
-            console.log('Scissors');
-            return 'Scissors';
+            humanchoice = 'Scissors'
+            return humanchoice;
         }
         else{
             console.log('Please enter Rock, Paper or Scissors'); 
             getHumanChoice(); //reprompt user if they enter an invalid choice
         }
                             }
-                            getHumanChoice()
+
+                getHumanChoice();
+                console.log('Human chose: ', humanchoice);
+                console.log('Computer chose: ', computerchoice);
+
+
+
+let humanScore = 0
+let computerScore = 0
+
+if (humanchoice == computerchoice)
+    {
+        console.log('Draw');
+        console.log(humanScore);
+        console.log(computerScore);
+    }
+else if(humanchoice === 'Rock' && computerchoice === 'Scissors')
+    {
+        console.log('Human wins');
+        humanScore ++;
+        console.log('Human score: ', humanScore);
+        console.log('Computer score: ', computerScore);
+    }
+else if(humanchoice === 'Paper' && computerchoice === 'Rock')
+    {
+        console.log('Human wins');
+        humanScore ++;
+        console.log('Human score: ', humanScore);
+        console.log('Computer score: ', computerScore);
+    }
+else if(humanchoice === 'Scissors' && computerchoice === 'Paper')
+    {
+        console.log('Human wins');
+        humanScore ++;
+        console.log('Human score: ', humanScore);
+        console.log('Computer score: ', computerScore);
+    }
+else
+    {
+        computerScore ++;
+        console.log('Human score: ', humanScore);
+        console.log('Computer score: ', computerScore);;
+    }
